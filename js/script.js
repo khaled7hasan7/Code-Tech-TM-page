9ad684afd250cc18fccf448f70412789c6971bb1
khaled <kh2662174@gmail.com>
2024-08-03 21:53:53 +0300

    added files
    added files

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide *100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function changeSlide(direction) {
+    showSlide(currentSlide + direction);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+});
+
+
+// function changeBackgroundColor() {
+//     const buttons = document.querySelectorAll('.prev, .next');
+//     buttons.forEach(button => {
+//         button.style.backgroundColor = 'black';
+//     });
+// }
